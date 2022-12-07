7782390ef6d40e6131eb787e6075ecd0e756570b
ArtemLevin <python_study_2022@mail.ru>
2022-12-07 12:06:38 +0300

    Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.Write($"{N} -> ");
+
+int counter = 1, cube;
+
+while (counter < (N + 1))
+{
+    cube = counter * counter * counter;
+    if (counter == N)
+    {
+        Console.Write($"{cube}"); // исключаем запятую после последнего куба
+        counter++;
+    }
+    else
+    {
+        Console.Write($"{cube}, ");
+        counter++;
+    }
+}
