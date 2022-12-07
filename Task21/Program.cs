fe3e06045ec347fe1af19728149041dd0f887e74
ArtemLevin <python_study_2022@mail.ru>
2022-12-07 11:57:08 +0300

    Task 21

---
+++ Task21/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Вводим координаты точки А");
+Console.Write("Введите координату икс: ");
+int xA =  int.Parse(Console.ReadLine()!); 
+Console.Write("Введите координату игрек: ");
+int yA =  int.Parse(Console.ReadLine()!); 
+Console.Write("Введите координату зет: ");
+int zA =  int.Parse(Console.ReadLine()!); 
+Console.WriteLine($"Координаты точки А({xA},{yA},{zA})");
+
+Console.WriteLine("Вводим координаты точки В");
+Console.Write("Введите координату икс: ");
+int xB =  int.Parse(Console.ReadLine()!); 
+Console.Write("Введите координату игрек: ");
+int yB =  int.Parse(Console.ReadLine()!); 
+Console.Write("Введите координату зет: ");
+int zB =  int.Parse(Console.ReadLine()!); 
+Console.WriteLine($"Координаты точки В({xB},{yB},{zB})");
+
+double distance = Math.Sqrt((xB-xA)*(xB-xA)+(yB-yA)*(yB-yA)+(zB-zA)*(zB-zA));
+
+Console.Write("Расстояние меджу точками " + distance );
+
+
